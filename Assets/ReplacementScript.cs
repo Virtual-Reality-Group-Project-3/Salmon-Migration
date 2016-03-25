ee44aa9eaa2c7c436e351d28c3f1ef84285a9795
mbregg <matthewbregg610@ufl.edu>
2016-03-25 13:32:01 -0400

    Mostly got ladder stuff

--- Assets/ReplacementScript.cs
+++ Assets/ReplacementScript.cs
@@ -2,11 +2,22 @@
 using System.Collections;
 
 public class ReplacementScript : MonoBehaviour {
+	public int ratePerSecond = 5;
+	private float timeCount;
+	private int spawned;
 	// Use this for initialization
 	void Start () {
 	
 	}
 	public void OnTriggerEnter(Collider collider) {
+		if (timeCount > 1) { 
+			timeCount = 0; 
+			spawned = 0;
+		}
+		if (spawned >= ratePerSecond) {
+			return;
+		}
+		++spawned;
 		Transform fishTransform; 
 		if (collider.gameObject.CompareTag ("fish from school")) {
 			Debug.Log ("Destroying! " + collider.gameObject.name);
@@ -22,6 +33,6 @@ public class ReplacementScript : MonoBehaviour {
 	}
 	// Update is called once per frame
 	void Update () {
-	
+			timeCount += Time.deltaTime;
 	}
 }
