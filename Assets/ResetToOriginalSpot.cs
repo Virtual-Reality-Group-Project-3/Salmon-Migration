dca4ae2004b674206d662de5cedd47497d3933a4
mbregg <matthewbregg610@ufl.edu>
2016-03-29 10:40:15 -0400

    Can get on/off the boatgit add --all

---
+++ Assets/ResetToOriginalSpot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetToOriginalSpot : MonoBehaviour {
+	public Transform original;
+	private Vector3 rotation;
+	private Vector3 position;
+	private Vector3 scale;
+	// Use this for initialization
+	void Start () {
+		scale = original.localScale;
+		position = original.position;
+		rotation = original.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
