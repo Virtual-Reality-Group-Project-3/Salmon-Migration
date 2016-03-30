8707ca8d0a20f9a0dfe92948ca2bce33b8b5d9d5
EvanMAmstutz <evangolf1@ufl.edu>
2016-03-30 09:41:34 -0400

    coin drop sound added and sequential with ticking sound

--- Assets/BinocularsScript.cs
+++ Assets/BinocularsScript.cs
@@ -9,6 +9,8 @@ public class BinocularsScript : MonoBehaviour {
 	private bool havePlayer=false;
 	public AudioClip tickingSound;
 	public AudioClip coinDropSound;
+	public float volume_coin=1;
+	public float volume_tick=1;
 
 
 
@@ -38,6 +40,7 @@ public class BinocularsScript : MonoBehaviour {
 	private void StartSound()
 	{
 		tickingAudio = SetUpAudioSource(tickingSound);
+		coinDropAudio = SetUpAudioSource (coinDropSound);
 	}
 
 	void toggleBinoView(bool toggle) {
@@ -80,7 +83,8 @@ public class BinocularsScript : MonoBehaviour {
 				toggleBinoView (!inBinoView);
 
 			}
-			tickingAudio.volume = 1;
+			coinDropAudio.volume = volume_coin;
+			tickingAudio.volume = volume_tick;
 		}
 
 	}
